ae7669dc9144599e08a568686b99d9b5168b83cd
Arseny Kostenko <119968546+arseny-kostenko@users.noreply.github.com>
2026-01-08 14:59:38 -0800

    Fixed parsing of fanout response
    Signed-off-by: Arseny Kostenko <119968546+arseny-kostenko@users.noreply.github.com>

--- apps/server/src/send-command.ts
+++ apps/server/src/send-command.ts
@@ -3,27 +3,29 @@ import WebSocket from "ws"
 import { VALKEY } from "../../../common/src/constants"
 import { parseResponse } from "./utils"
 
+export const isRequestError = (x: unknown): x is Error | string =>
+  x instanceof Error ||
+  (typeof x === "string" && x.startsWith("ResponseError:"))
+
 export async function sendValkeyRunCommand(
   client: GlideClient | GlideClusterClient,
   ws: WebSocket,
   payload: { command: string; connectionId: string },
 ) {
   try {
-    let response = (await client.customCommand(
-      payload.command.split(" "),
-    ))
-    if (typeof response === "string") {
-      if (response.includes("ResponseError")) {
-        ws.send(
-          JSON.stringify({
-            meta: { command: payload.command },
-            type: VALKEY.COMMAND.sendFailed,
-            payload: response,
-          }),
-        )
-      }
-      response = parseResponse(response)
+    const response = await client.customCommand(payload.command.split(" "))
+
+    if (isRequestError(response)) {
+      ws.send(
+        JSON.stringify({
+          meta: { command: payload.command },
+          type: VALKEY.COMMAND.sendFailed,
+          payload: response,
+        }),
+      )
     }
+
+    const parsed = parseResponse(response)
 
     ws.send(
       JSON.stringify({
@@ -32,7 +34,7 @@ export async function sendValkeyRunCommand(
           command: payload.command,
         },
         type: VALKEY.COMMAND.sendFulfilled,
-        payload: response,
+        payload: parsed,
       }),
     )
   } catch (err) {
